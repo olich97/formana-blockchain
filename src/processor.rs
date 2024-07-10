5d2b7bee057a483deb3907736a179e85c74bf45a
Oleh Andrushko <olich.me@gmail.com>
2024-07-10 17:01:06 +0200

    Add some comments

--- src/processor.rs
+++ src/processor.rs
@@ -35,6 +35,18 @@ impl Processor {
         }
     }
 
+    /// Processes the `CreateForm` instruction.
+    ///
+    /// # Arguments
+    ///
+    /// * `accounts` - The accounts required for the instruction.
+    /// * `code` - The code of the form.
+    /// * `schema_url` - The URL of the schema.
+    /// * `program_id` - The ID of the program.
+    ///
+    /// # Returns
+    ///
+    /// A `ProgramResult` indicating the success or failure of the instruction processing.
     fn process_create_form(
         accounts: &[AccountInfo],
         code: String,
@@ -42,29 +54,35 @@ impl Processor {
         program_id: &Pubkey,
     ) -> ProgramResult {
         let account_info_iter = &mut accounts.iter();
-        // aka Form creator account
-        let authority_account = next_account_info(account_info_iter)?;
-        // Account derived PDA
-        let form_account = next_account_info(account_info_iter)?;
-        //
-        let system_program = next_account_info(account_info_iter)?;
-         // Fee payer account
-         let payer_account = next_account_info(account_info_iter)?; 
 
-        // validations
+        // Get the form creator account
+        let authority_account = next_account_info(account_info_iter)?;
+
+        // Get the target form account PDA
+        let form_account = next_account_info(account_info_iter)?;
+
+        let system_program = next_account_info(account_info_iter)?;
+
+        // Get the fee payer account
+        let payer_account = next_account_info(account_info_iter)?;
+
+        // Form creator account must be a signer
         if !authority_account.is_signer {
             return Err(ProgramError::MissingRequiredSignature);
         }
-        // Form PDA is derived by using seeds: form code and form creator address
+
+        // Determine the form account PDA
         let (form_pda, form_bump) = Pubkey::find_program_address(
             &[authority_account.key.as_ref(), code.as_ref()],
             program_id,
         );
 
+        // Check if the derived form account matches the passed account
         if form_pda != *form_account.key {
             return Err(FormanaError::InvalidFormAccount.into());
         }
 
+        // Calculate the size of the form account and rent details
         let form_size = Form::get_account_size(&code, &schema_url);
         let rent = Rent::get()?;
         let rent_lamports = rent.minimum_balance(form_size);
@@ -77,6 +95,7 @@ impl Processor {
             program_id,
         );
 
+        // Invoke the signed transaction to actually create the PDA account
         invoke_signed(
             create_form_pda_ix,
             &[
@@ -86,42 +105,67 @@ impl Processor {
             ],
             &[&[authority_account.key.as_ref(), code.as_ref(), &[form_bump]]],
         )?;
+
         let mut form_data = try_from_slice_unchecked::<Form>(&form_account.data.borrow()).unwrap();
+        // Populate the form data
         form_data.creator = *authority_account.key;
         form_data.schema_url = schema_url;
         form_data.bump = form_bump;
         form_data.code = code;
 
+        // Serialize the form data into the account data
         form_data.serialize(&mut &mut form_account.data.borrow_mut()[..])?;
         Ok(())
     }
 
+    /// Processes the creation of a submission.
+    ///
+    /// # Arguments
+    ///
+    /// * `accounts` - The accounts passed to the program.
+    /// * `content_url` - The URL of the content being submitted.
+    /// * `program_id` - The ID of the program.
+    ///
+    /// # Returns
+    ///
+    /// A `ProgramResult` indicating the success or failure of the operation.
     fn process_create_submission(
         accounts: &[AccountInfo],
         content_url: String,
         program_id: &Pubkey,
     ) -> ProgramResult {
+        // Get the account iterators
         let account_info_iter = &mut accounts.iter();
-        // aka submission creator account
-        let authority_account = next_account_info(account_info_iter)?;
-        // Account derived PDA for target form
-        let form_account = next_account_info(account_info_iter)?;
-        // Account derived PDA
-        let submission_account = next_account_info(account_info_iter)?;
-        let system_program = next_account_info(account_info_iter)?;
-        // Fee payer account
-        let payer_account = next_account_info(account_info_iter)?; 
 
+        // Get the submission creator account
+        let authority_account = next_account_info(account_info_iter)?;
+
+        // Get the target form account
+        let form_account = next_account_info(account_info_iter)?;
+
+        // Get the submission account derived from the PDA
+        let submission_account = next_account_info(account_info_iter)?;
+
+        // Get the system program account
+        let system_program = next_account_info(account_info_iter)?;
+
+        // Get the fee payer account
+        let payer_account = next_account_info(account_info_iter)?;
+
+        // Check if the authority account is a signer
         if !authority_account.is_signer {
             return Err(ProgramError::MissingRequiredSignature);
         }
 
+        // Check if the form account is writable and already initialized (not empty)
         if !form_account.is_writable || form_account.data_is_empty() {
             return Err(FormanaError::InvalidFormAccount.into());
         }
 
+        // Get the form data from the form account
         let form_data = try_from_slice_unchecked::<Form>(&form_account.data.borrow()).unwrap();
 
+        // Derive the submission account PDA
         let (submission_pda, submission_bump) = Pubkey::find_program_address(
             &[
                 authority_account.key.as_ref(),
@@ -131,10 +175,12 @@ impl Processor {
             program_id,
         );
 
+        // Check if the derived submission account matches the passed account
         if submission_pda != *submission_account.key {
             return Err(FormanaError::InvalidSubmissionAccount.into());
         }
 
+        // Calculate the size of the submission account and rent details
         let submission_size = Submission::get_account_size(&content_url);
         let rent = Rent::get()?;
         let rent_lamports = rent.minimum_balance(submission_size);
@@ -147,6 +193,7 @@ impl Processor {
             program_id,
         );
 
+        // Invoke the signed transaction to actually create the PDA account
         invoke_signed(
             create_submission_pda_ix,
             &[
@@ -162,16 +209,23 @@ impl Processor {
             ]],
         )?;
 
+        // Get the current clock timestamp
         let clock = Clock::get()?;
+
+        // Initialize the submission data
         let mut submission_data =
             try_from_slice_unchecked::<Submission>(&submission_account.data.borrow()).unwrap();
+
+        // Populate the submission data
         submission_data.form = *form_account.key;
         submission_data.author = *authority_account.key;
         submission_data.content_url = content_url;
         submission_data.bump = submission_bump;
         submission_data.timestamp = clock.unix_timestamp as u64;
 
+        // Serialize the submission data into the account data
         submission_data.serialize(&mut &mut submission_account.data.borrow_mut()[..])?;
+
         Ok(())
     }
 }
